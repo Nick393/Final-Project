06edf2f0f3cb141d38284369a5901c3ec7196d07
LeTegu <milesvsierra@gmail.com>
2022-11-15 09:27:43 -0600

    I fixed it

---
+++ Final Project/CharacterTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace Final_Project
+{
+    public class CharacterTemplate
+    {
+
+        private string name;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+        public CharacterTemplate()
+        {
+            name = "";
+        }
+    }
+}
--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -7,6 +7,9 @@ namespace Final_Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            CharacterTemplate Tom = new CharacterTemplate();
+            Tom.setName("Kevin");
+            Console.WriteLine(Tom.getName());
         }
     }
 }
